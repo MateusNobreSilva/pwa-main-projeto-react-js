2cc7e94d7cc9f617f660e7666bb37d90afb9ecce
MateusNobreSilva <mateusnobresilva@gmail.com>
2022-10-07 08:36:22 -0300

    alterações 07/10/2022

--- src/index.tsx
+++ src/index.tsx
@@ -23,11 +23,12 @@ serviceWorkerRegistration.register();
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('../firebase-messaging-sw.js')
-    .then(function (registration) {
-      console.log('Registration successful, scope is:', registration.scope);
+    .then(registration => {
+      return registration;
+      // console.log('Registration successful, scope is:', registration.scope);
     })
-    .catch(function (err) {
-      console.log('Service worker registration failed, error:', err);
+    .catch(() => {
+      // console.log('Service worker registration failed, error:', err);
     });
 }
 
